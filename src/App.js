ce17cb472b0929eea634f32ba7fb2a530997cd0e
Anh Le <kunquan2345@gmail.com>
2019-07-12 23:13:23 -0700

    Solve the bug happen in App.js That happen due to similar variable name exercises, combined with hoisting, lead to confusion where JavaScript search for the variable inside the function first before search outside Two same named variable lead to confusion, as JavaScript will take inside variable first, even when it haven't initialized yet

--- src/App.js
+++ src/App.js
@@ -2,23 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 import { Header, Footer } from "./components/layouts";
 import { muscles, exercises } from "./store";
-import Excercise from "./components/excercises/index";
-//let initial = exercises;
+import Exercise from "./components/excercises/index";
 function App() {
-  // Error: Cannot access 'exercises' before initialization
-  // Guess bug in the line below
-  // Strange is, if paste the line below to outside the scope of function, it work well
-  // First guess is something is wrong with inport, but prove it's wrong as muscles still work
-  // Second guess is because the hoisting feature
-  // As in a function scope, when doing stuff with a variable, it first (guessing) search for the vriable with that name inside the function first
-  // If can't find, then it will go outer scope
-  console.log(exercises);
-
-  const [exercises, setExercises] = useState();
+  const [exercise, setExercises] = useState(exercises);
   return (
     <div className="App">
       <Header />
-      <Excercise />
+      <Exercise />
       <Footer muscles={muscles} />
     </div>
   );
