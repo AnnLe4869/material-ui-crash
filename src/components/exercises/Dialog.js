3c3a78a283db2e3014c29fc85794cb51d5cdcf19
Anh Le <kunquan2345@gmail.com>
2019-07-21 02:58:40 -0700

    Start using high-order component, a special component that return a new component A bug appear. When edit/create a new item, can't create/edit other item Guess because the Context passed down incomplete

--- src/components/exercises/Dialog.js
+++ src/components/exercises/Dialog.js
@@ -8,37 +8,34 @@ import {
 } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import Form from "./Form";
-import { Consumer } from "../../context";
-
-export default function CreateDialog({ onCreate }) {
+import { Consumer, withContext } from "../../context";
+function CreateDialog(props) {
   const [open, setOpen] = useState(false);
-
+  const { muscles, onCreate } = props;
   function handleClick() {
     setOpen(!open);
   }
 
   return (
-    <Consumer>
-      {({ muscles, onCreate }) => (
-        <React.Fragment>
-          <Fab color="secondary" onClick={handleClick} size="small">
-            <AddIcon />
-          </Fab>
-          <Dialog open={open} onClose={handleClick} maxWidth="sm" fullWidth>
-            <DialogTitle>Create a new exercise</DialogTitle>
-            <DialogContent>
-              <DialogContentText>Please fill out the form below</DialogContentText>
-              <Form
-                muscles={muscles}
-                onSubmit={exercise => {
-                  onCreate(exercise);
-                  handleClick();
-                }}
-              />
-            </DialogContent>
-          </Dialog>
-        </React.Fragment>
-      )}
-    </Consumer>
+    <React.Fragment>
+      <Fab color="secondary" onClick={handleClick} size="small">
+        <AddIcon />
+      </Fab>
+      <Dialog open={open} onClose={handleClick} maxWidth="sm" fullWidth>
+        <DialogTitle>Create a new exercise</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Please fill out the form below</DialogContentText>
+          <Form
+            muscles={muscles}
+            onSubmit={exercise => {
+              onCreate(exercise);
+              handleClick();
+            }}
+          />
+        </DialogContent>
+      </Dialog>
+    </React.Fragment>
   );
 }
+
+export default withContext(CreateDialog);
--- src/context.js
+++ src/context.js
@@ -1,3 +1,9 @@
-import { createContext } from "react";
+import React, { createContext } from "react";
 
 export const { Provider, Consumer } = createContext();
+
+// withContext() receive a component and return a new component.
+// From props => ... is a functional component that is created
+export const withContext = Component => props => (
+  <Consumer>{value => <Component {...value} {...props} />}</Consumer>
+);
