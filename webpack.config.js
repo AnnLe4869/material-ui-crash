7e651e99b9dc6e90bb8066c188681ef208094bd1
Anh Le <kunquan2345@gmail.com>
2020-01-01 14:58:10 -0800

    Set up webpack config for server file
    - Need to set up external so to exclude all package from being bundle into one file
    - Need to set up the target, which is node

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,9 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
-module.exports = {
-  entry: "./src/client",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    contentBase: "./dist"
-  },
+const nodeExternals = require("webpack-node-externals");
+
+const commonConfig = {
   devtool: "source-map",
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
@@ -20,12 +14,29 @@ module.exports = {
     alias: {
       "@material-ui/core": "@material-ui/core/es"
     }
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html"
-    }),
-    new BundleAnalyzerPlugin()
-  ]
+  }
 };
+
+module.exports = [
+  {
+    ...commonConfig,
+    entry: "./src/client",
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "public")
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html"
+      }),
+      new BundleAnalyzerPlugin()
+    ]
+  },
+  {
+    ...commonConfig,
+    target: "node",
+    entry: "./src/server",
+    externals: [nodeExternals()]
+  }
+];
